24479c99be9eef821798565a8bf9fca8e9c827e5
Matheus Esteves Zanoto <matheusesteveszanoto@gmail.com>
2017-10-15 16:01:41 -0200

    finalizacao - lab05

---
+++ lab05/arq01.res
@@ -0,0 +1,133 @@
+Foram inseridos com sucesso 5 livros na prateleira de custo $31
+Foram inseridos com sucesso 3 livros na prateleira de custo $46
+A reorganização da prateleira 5 foi feita com sucesso
+[VALOR TOTAL DA ESTANTE $293] dos quais:
+Prateleira 0: $0
+Prateleira 1: $0
+Prateleira 2: $138
+Prateleira 3: $0
+Prateleira 4: $155
+Prateleira 5: $0
+A mudança de livros : [6, $46] ==> [4, $46] foi feita com sucesso
+Foram inseridos com sucesso 11 livros na prateleira de custo $19
+A reorganização da prateleira 4 foi feita com sucesso
+Foram inseridos com sucesso 1 livros na prateleira de custo $31
+[VALOR TOTAL DA ESTANTE $533] dos quais:
+Prateleira 0: $0
+Prateleira 1: $0
+Prateleira 2: $138
+Prateleira 3: $209
+Prateleira 4: $186
+Prateleira 5: $0
+Nao existe nenhum livro na posicao desejada
+Foram inseridos com sucesso 1 livros na prateleira de custo $49
+Foram inseridos com sucesso 2 livros na prateleira de custo $16
+A reorganização da prateleira 4 foi feita com sucesso
+[LIVROS DA PRATELEIRA 1]
+Coluna 0: [Livro_jypck0v4l] [Livro_0mf4lypfr]
+Coluna 1:
+Coluna 2:
+Coluna 3:
+Coluna 4:
+Coluna 5:
+Coluna 6:
+Foram inseridos com sucesso 14 livros na prateleira de custo $10
+[LIVROS DA PRATELEIRA 4]
+Coluna 0: [Livro_ioni7yszautvm]
+Coluna 1: [Livro_hcc] [Livro_8yh] [Livro_0v4] [Livro_8b2] [Livro_ace]
+Coluna 2:
+Coluna 3:
+Coluna 4:
+Coluna 5:
+Coluna 6:
+[VALOR TOTAL DA ESTANTE $754] dos quais:
+Prateleira 0: $49
+Prateleira 1: $32
+Prateleira 2: $138
+Prateleira 3: $209
+Prateleira 4: $186
+Prateleira 5: $140
+Foram inseridos com sucesso 1 livros na prateleira de custo $31
+Foram inseridos com sucesso 12 livros na prateleira de custo $46
+Foram inseridos com sucesso 17 livros na prateleira de custo $46
+Foram inseridos com sucesso 17 livros na prateleira de custo $31
+Foram inseridos com sucesso 6 livros na prateleira de custo $46
+Foram inseridos com sucesso 17 livros na prateleira de custo $16
+A reorganização da prateleira 0 foi feita com sucesso
+A mudança de livros : [5, $46] ==> [1, $19] foi feita com sucesso
+Foram inseridos com sucesso 14 livros na prateleira de custo $46
+Foram inseridos com sucesso 11 livros na prateleira de custo $31
+Foram inseridos com sucesso 6 livros na prateleira de custo $46
+Foram inseridos com sucesso 15 livros na prateleira de custo $19
+Foram inseridos com sucesso 14 livros na prateleira de custo $49
+A mudança de livros : [2, $31] ==> [3, $49] foi feita com sucesso
+Foram inseridos com sucesso 11 livros na prateleira de custo $19
+Espaco insuficiente na prateleira de custo $46. Favor remover os livros do compartimento auxiliar.
+[VALOR TOTAL DA ESTANTE $5797] dos quais:
+Prateleira 0: $1176
+Prateleira 1: $304
+Prateleira 2: $2668
+Prateleira 3: $703
+Prateleira 4: $806
+Prateleira 5: $140
+A reorganização da prateleira 4 foi feita com sucesso
+A mudança de livros : [5, $10] ==> [2, $19] foi feita com sucesso
+Foram inseridos com sucesso 13 livros na prateleira de custo $31
+A reorganização da prateleira 0 foi feita com sucesso
+Foram inseridos com sucesso 11 livros na prateleira de custo $19
+A reorganização da prateleira 1 foi feita com sucesso
+[VALOR TOTAL DA ESTANTE $6409] dos quais:
+Prateleira 0: $1176
+Prateleira 1: $304
+Prateleira 2: $2668
+Prateleira 3: $912
+Prateleira 4: $1209
+Prateleira 5: $140
+Foram inseridos com sucesso 7 livros na prateleira de custo $10
+Espaco insuficiente na prateleira de custo $46. Favor remover os livros do compartimento auxiliar.
+A mudança de livros : [1, $46] ==> [3, $10] foi feita com sucesso
+Foram inseridos com sucesso 17 livros na prateleira de custo $16
+[VALOR TOTAL DA ESTANTE $6427] dos quais:
+Prateleira 0: $1176
+Prateleira 1: $576
+Prateleira 2: $2254
+Prateleira 3: $912
+Prateleira 4: $1209
+Prateleira 5: $300
+A reorganização da prateleira 3 foi feita com sucesso
+[LIVROS DA PRATELEIRA 4]
+Coluna 0: [Livro_z8buey00] [Livro_42c4p254] [Livro_7sic4kku] [Livro_astzwykp] [Livro_9axc083i] [Livro_hub883hp] [Livro_8edh6ess] [Livro_7oin4ri8] [Livro_25lti6na]
+Coluna 1: [Livro_ds4dvkux] [Livro_afx4nn5b] [Livro_swude3mq] [Livro_00vidzmn] [Livro_ise92a4c] [Livro_m6vqh744] [Livro_5goypq5i] [Livro_spnk6ap5] [Livro_1p18qwxe]
+Coluna 2: [Livro_qxsu0hbu] [Livro_sfjgyi02] [Livro_24esdgqz]
+Coluna 3:
+Coluna 4: [Livro_rromum1] [Livro_sa45f35] [Livro_1gc4aha] [Livro_yjl9v0l] [Livro_hcc] [Livro_8yh] [Livro_0v4] [Livro_8b2] [Livro_ace]
+Coluna 5: [Livro_3iatqpb] [Livro_rptn0f7] [Livro_pgbhklc] [Livro_wdxp64h] [Livro_qpjm7ii] [Livro_tp5ofaa] [Livro_2gwhkeo] [Livro_cwl7] [Livro_ioni7yszautvm]
+Coluna 6:
+Foram inseridos com sucesso 3 livros na prateleira de custo $16
+Foram inseridos com sucesso 9 livros na prateleira de custo $46
+Livro Livro_fgjlr removido com sucesso da coluna 2 da prateleira de custo $19
+Foram inseridos com sucesso 8 livros na prateleira de custo $49
+Nao existe nenhum livro na posicao desejada
+Foram inseridos com sucesso 1 livros na prateleira de custo $31
+Nao existe nenhum livro na posicao desejada
+A reorganização da prateleira 0 foi feita com sucesso
+Foram inseridos com sucesso 11 livros na prateleira de custo $49
+A mudança de livros : [6, $49] ==> [6, $19] foi feita com sucesso
+Foram inseridos com sucesso 6 livros na prateleira de custo $31
+[VALOR TOTAL DA ESTANTE $7778] dos quais:
+Prateleira 0: $1715
+Prateleira 1: $624
+Prateleira 2: $2668
+Prateleira 3: $1045
+Prateleira 4: $1426
+Prateleira 5: $300
+Foram inseridos com sucesso 9 livros na prateleira de custo $31
+Foram inseridos com sucesso 7 livros na prateleira de custo $31
+Foram inseridos com sucesso 5 livros na prateleira de custo $19
+Espaco insuficiente na prateleira de custo $31. Favor remover os livros do compartimento auxiliar.
+Espaco insuficiente na prateleira de custo $31. Favor remover os livros do compartimento auxiliar.
+A reorganização da prateleira 1 foi feita com sucesso
+Espaco insuficiente na prateleira de custo $46. Favor remover os livros do compartimento auxiliar.
+Foram inseridos com sucesso 16 livros na prateleira de custo $49
+Foram inseridos com sucesso 3 livros na prateleira de custo $46
+Espaco insuficiente na prateleira de custo $31. Favor remover os livros do compartimento auxiliar.
--- lab05/lab05.c
+++ lab05/lab05.c
@@ -1,10 +1,38 @@
+/*
+  Nome: Matheus Esteves Zanoto   RA: 184256
+
+  Objetivos: Gerenciar a execução de operações de uma estante com o apoio de um compartimento auxiliar. A
+  estante deve efetuar operações específicas, dentre as quais três são as mais importantes: inserir uma
+  nova remessa de livros em uma determinada prateleira, remover um determinado livro numa determinada 
+  localização e reorganizar uma prateleira inteira dada uma nova configuração.
+
+  Entradas: Receberá como entrada o número da operação correspondente seguido dos parâmetros necessários, 
+  que podem ser nomes de livros a serem inseridos numa determinada prateleira informada ou uma determinada
+  localização na estante (coluna, prateleira ou uma determinada altura na pilha de livros) para as demais
+  operações.
+
+  Saída: Terá como saída mensagens que informam o sucesso ou não em relação à realização da operação desejada,
+  informando os dados relativos à operação (informações acerca do livro sobre o qual a operação foi realizada ou 
+  uma determinada localização), relatório de todos os livros da estante ou o relatório do montante relacionado 
+  aos livros da estante.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+/*
+  Estrutura para armazenar um determinado livro na pilha de livros.
+*/
 typedef struct{
   char* nome;
 } Livro;
 
+/*
+  Estrutura que representa uma pilha de vários livros, cujo tamanho máximo
+  permitido é armazenado. Há um vetor que armazena um ponteiro para um 
+  determinado livro em cada posição.
+*/
 typedef struct{
   Livro** livros;
   int tamanho;
@@ -26,18 +54,22 @@ typedef struct{
 
 void inicializarPrateleira(Prateleira* prateleira, int colunas, int limite);
 void adicionarLivros(Prateleira* prateleiras, Pilha* aux, int prat);
-void imprimirLivros(Prateleira* prateleiras, int prat);
-void mudarPrecos(Prateleira* prateleiras, int prat_origem, int col_origem, int prat_destino, int col_destino);
+void imprimirLivros(Prateleira* prateleiras, int prat, Pilha* aux);
+void mudarPrecos(Prateleira* prateleiras, int prat_origem, int col_origem, int prat_destino, int col_destino, Pilha* aux);
 void calcularMontante(Prateleira* prateleiras, int qtdPrateleiras);
-void reorganizarPrateleira(Prateleira* prateleiras, int prat, int* configuracao);
-Livro* removerLivro(Prateleira* prateleiras, int p, int c, int l);
+void reorganizarPrateleira(Prateleira* prateleiras, int prat, int* configuracao, Pilha* aux);
+Livro* removerLivro(Prateleira* prateleiras, int p, int c, int l, Pilha* aux);
 
 int main(){
   int p,c,l;
   scanf("%d %d %d\n",&p,&c,&l);
   
+  /*
+    Inicializamos uma estante, com base num vetor de prateleiras, cujo tamanho
+    é informado como entrada do programa.
+  */
   Prateleira* prateleiras = (Prateleira*)malloc(p*sizeof(Prateleira));
-  int i,j;
+  int i;
   for (i = 0; i < p-1; i++){
     scanf("%f ",&(prateleiras[i].preco));
     inicializarPrateleira(&(prateleiras[i]),c,l);
@@ -45,6 +77,10 @@ int main(){
   scanf("%f\n",&(prateleiras[i].preco));
   inicializarPrateleira(&(prateleiras[i]),c,l);
 
+  /*
+    Compartimento auxiliar para livros, por meio do qual realizamos várias
+    operações relacionadas aos livros no programa.
+  */
   Pilha aux;
   inicializarPilha(&aux,2*l);
 
@@ -54,6 +90,7 @@ int main(){
       case 1:{
         int prat,qtd;
         scanf("%d %d ",&prat,&qtd);
+        liberarPilha(&aux);
         for (i = 0; i < qtd-1; i++){
           Livro livro;
           livro.nome = (char*)malloc(30*sizeof(char));
@@ -66,10 +103,9 @@ int main(){
         empilhar(&aux,livro);
 
         if (aux.tamanho > (c*l - prateleiras[prat].qtdLivros)){
-          printf("Espaco insuficiente na prateleira de custo $%.0f. Favor remover os livros do compartimento auxiliar\n",
+          printf("Espaco insuficiente na prateleira de custo $%.0f. Favor remover os livros do compartimento auxiliar.\n",
           	      prateleiras[prat].preco);
-          while (aux.tamanho > 0)
-            desempilhar(&aux);
+          liberarPilha(&aux);
         }
         else{
           adicionarLivros(&(*prateleiras),&aux,prat);
@@ -77,14 +113,15 @@ int main(){
         }
       } break;
       case 2:{
-        int p,c,l;
-        scanf("%d %d %d",&p,&c,&l);
-        Livro* livroRemovido = removerLivro(&(*prateleiras),p,c,l);
+        int prat,coluna,altura;
+        scanf("%d %d %d",&prat,&coluna,&altura);
+        liberarPilha(&aux);
+        Livro* livroRemovido = removerLivro(&(*prateleiras),prat,coluna,altura,&aux);
         if (livroRemovido == NULL)
           printf("Nao existe nenhum livro na posicao desejada\n");
         else
           printf("Livro %s removido com sucesso da coluna %d da prateleira de custo $%.0f\n",
-            livroRemovido->nome,c,prateleiras[p].preco
+            livroRemovido->nome,coluna,prateleiras[prat].preco
           );
       } break;
       case 3:{
@@ -97,8 +134,9 @@ int main(){
         if (origem.tamanho > destino.limite - destino.tamanho)
           printf("Espaco insuficiente na prateleira %d e coluna %d\n",prat_destino,col_destino);
         else{
-          mudarPrecos(&(*prateleiras),prat_origem,col_origem,prat_destino,col_destino);
-          printf("A mudanca de livros : [%d, $%.0f] ==> [%d, $%.0f] foi feita com sucesso\n",
+          liberarPilha(&aux);
+          mudarPrecos(&(*prateleiras),prat_origem,col_origem,prat_destino,col_destino,&aux);
+          printf("A mudança de livros : [%d, $%.0f] ==> [%d, $%.0f] foi feita com sucesso\n",
             col_origem,pratOrigem.preco,col_destino,pratDestino.preco
           );
         }
@@ -110,8 +148,9 @@ int main(){
         configuracao = (int*)malloc(c*sizeof(int));
         for (i = 0; i < c; i++)
           scanf("%d",&(configuracao[i]));
-        reorganizarPrateleira(&(*prateleiras),prat,&(*configuracao));
-        printf("A reorganizacao da prateleira %d foi feita com sucesso\n",prat);
+        liberarPilha(&aux);
+        reorganizarPrateleira(&(*prateleiras),prat,&(*configuracao),&aux);
+        printf("A reorganização da prateleira %d foi feita com sucesso\n",prat);
       } break;
       case 5:{
         calcularMontante(&(*prateleiras),p);
@@ -119,7 +158,8 @@ int main(){
       default:{
         int prat;
         scanf("%d",&prat);
-        imprimirLivros(&(*prateleiras),prat);
+        liberarPilha(&aux);
+        imprimirLivros(&(*prateleiras),prat,&aux);
       }
     }
   }
@@ -127,6 +167,12 @@ int main(){
   return 0;
 }
 
+/*
+  Alocamos um vetor de ponteiro de livros na pilha informada,
+  cujo tamanho máximo permitido também é informado. Inicialmente,
+  em cada posição desse vetor, o ponteiro que aponta para um
+  determinado livro é nulo, ou seja, não há livro naquela posição.
+*/
 void inicializarPilha(Pilha* pilha, int limite){
   pilha->tamanho = 0;
   pilha->limite  = limite;
@@ -137,29 +183,47 @@ void inicializarPilha(Pilha* pilha, int limite){
 }
 
 void liberarPilha(Pilha* pilha){
-  free(pilha->livros);
+  while (pilha->tamanho > 0)
+    desempilhar(&(*pilha));
 }
 
+/*
+  Criamos um ponteiro para um determinado livro passado como parâmetro
+  no final do nosso vetor de livros, ou seja, empilhamos um ponteiro 
+  para um determinado livro na pilha.
+*/
 void empilhar(Pilha* pilha, Livro livro){
   pilha->livros[pilha->tamanho] = (Livro*)malloc(sizeof(Livro));
-  *(pilha->livros[pilha->tamanho]) = livro;
+  pilha->livros[pilha->tamanho]->nome = (char*)malloc(30*sizeof(char));
+  strcpy(pilha->livros[pilha->tamanho]->nome,livro.nome);
   pilha->tamanho++;
 }
 
+/*
+  Desalocamos o ponteiro para o último livro no nosso vetor de livros,
+  ou seja, desempilhamos um ponteiro de livro da nossa pilha. Retornamos
+  um ponteiro para o livro desempilhado.
+*/
 Livro* desempilhar(Pilha* pilha){
   if (pilha->tamanho > 0){
-    Livro livroDesempilhado = *(pilha->livros[pilha->tamanho-1]);
+    Livro* livroDesempilhado = (Livro*)malloc(sizeof(Livro));
+    Livro* ponteiroLivro = pilha->livros[pilha->tamanho-1];
+    livroDesempilhado->nome = (char*)malloc(30*sizeof(char));
+    strcpy(livroDesempilhado->nome,ponteiroLivro->nome);
     free(pilha->livros[pilha->tamanho-1]);
     pilha->livros[pilha->tamanho-1] = NULL;
     pilha->tamanho--;
-    Livro livro;
-    Livro* ponteiroLivro = &livro;
-    *ponteiroLivro = livroDesempilhado;
-    return ponteiroLivro;
+    return &(*livroDesempilhado);
   }
   return NULL;  
 }
 
+/*
+  Inicializamos todas as pilhas em cada coluna da nossa prateleira, utilizando o método
+  para inicializar uma determinada pilha descrito anteriormente. A nossa prateleira 
+  também armazenará a quantidade de livros empilhados até o momento, considerando
+  todas as suas colunas.
+*/
 void inicializarPrateleira(Prateleira* prateleira, int colunas, int limite){
   prateleira->qtdLivros = 0;
   prateleira->colunas = colunas;
@@ -170,116 +234,132 @@ void inicializarPrateleira(Prateleira* prateleira, int colunas, int limite){
     inicializarPilha(&(prateleira->pilhas[i]),limite);
 }
 
+/*
+  Adicionamos uma nova remessa de livros na nossa prateleira cujo índice foi informado.
+  O programa preenche sequencialmente em cada uma das colunas da prateleira, começando 
+  da coluna zero. Ou seja, caso não haja mais espaço na coluna 0, irá preencher na coluna 1
+  e assim por diante, até que todos os livros do nosso compartimento auxiliar sejam desempilhados
+  e alocados na prateleira informada.
+*/
 void adicionarLivros(Prateleira* prateleiras, Pilha* aux, int prat){
   int coluna = 0;
-  int qtdLivros = aux->tamanho;
   while (aux->tamanho > 0){
-    Pilha* pilha = &(prateleiras[prat].pilhas[coluna++]);
-    while (aux->tamanho > 0 && pilha->tamanho < pilha->limite){
+    while (aux->tamanho > 0 && prateleiras[prat].pilhas[coluna].tamanho < prateleiras[prat].pilhas[coluna].limite){
        Livro* livro = desempilhar(&(*aux));
-       empilhar(pilha,*livro);
+       empilhar(&(prateleiras[prat].pilhas[coluna]),*livro);
+       prateleiras[prat].qtdLivros++;
     }
+    coluna++;
   }
-  prateleiras[prat].qtdLivros += qtdLivros;
 }
 
-void imprimirLivros(Prateleira* prateleiras, int prat){
+/*
+  Exibimos todos os livros armazenados em cada uma das colunas de cada uma das prateleiras
+  da nossa estante, utilizando o compartimento auxiliar para exibir os livros na ordem
+  correta (ordem de empilhamento).
+*/
+void imprimirLivros(Prateleira* prateleiras, int prat, Pilha* aux){
   printf("[LIVROS DA PRATELEIRA %d]\n",prat);
   Prateleira* prateleira = &(prateleiras[prat]);
   int i;
   for (i = 0; i < prateleira->colunas; i++){
     printf("Coluna %d:",i);
     Pilha livros = prateleira->pilhas[i];
-    Pilha aux;
-    inicializarPilha(&aux,prateleira->limite);
     while (livros.tamanho > 0){
       Livro* livro = desempilhar(&livros);
-      printf("[%s]",livro->nome);
-      empilhar(&aux,*livro);
+      printf(" [%s]",livro->nome);
+      empilhar(&(*aux),*livro);
     }
-    while (aux.tamanho > 0){
-      Livro* livro = desempilhar(&aux);
+    while (aux->tamanho > 0){
+      Livro* livro = desempilhar(&(*aux));
       empilhar(&livros,*livro);
     }
     printf("\n");
   }
 }
 
-Livro* removerLivro(Prateleira* prateleiras, int p, int c, int l){
-  Prateleira* prateleira = &(prateleiras[p]);
-  Pilha* livros = &(prateleira->pilhas[c]);
-  if (l > livros->tamanho-1)
+/*
+  Removemos um determinado livro de uma determinada prateleira, numa determinada
+  coluna, cuja altura em relação ao primeiro livro empilhado na coluna foi informada.
+  Caso essa altura seja maior que o tamanho da pilha, não deixamos remover, pois essa
+  posĩção não existe na coluna. Caso contŕario, desempilhamos todos os livros acima
+  do livro que queremos remover, desempilhamos esse livro da nossa coluna e em seguida
+  empilhamos novamente os livros desempilhados anteriormente na mesma coluna.
+*/
+Livro* removerLivro(Prateleira* prateleiras, int p, int c, int l, Pilha* aux){
+  if (l > prateleiras[p].pilhas[c].tamanho-1)
     return NULL;
-  Pilha aux;
-  inicializarPilha(&aux,prateleira->limite);
-  while (aux.tamanho < livros->limite - (l + 1)){
-    Livro* livro = desempilhar(&(*livros));
-    empilhar(&aux,*livro);
+  int tamanhoOriginal = prateleiras[p].pilhas[c].tamanho;
+  while (aux->tamanho < tamanhoOriginal - (l + 1)){
+    Livro* livro = desempilhar(&(prateleiras[p].pilhas[c]));
+    empilhar(&(*aux),*livro);
   }
-  Livro* livro = desempilhar(&(*livros));
-  while (aux.tamanho > 0){
-    Livro* movido = desempilhar(&aux);
-    empilhar(&(*livros),*livro);
+  Livro* livro = desempilhar(&(prateleiras[p].pilhas[c]));
+  while (aux->tamanho > 0){
+    Livro* movido = desempilhar(&(*aux));
+    empilhar(&(prateleiras[p].pilhas[c]),*movido);
   }
-  Livro conteudoLivro;
-  Livro* ponteiroLivro = &conteudoLivro;
-  *ponteiroLivro = *livro;
-  return ponteiroLivro;
+  prateleiras[p].qtdLivros--;
+  return &(*livro);
 }
 
-void mudarPrecos(Prateleira* prateleiras, int prat_origem, int col_origem, int prat_destino, int col_destino){
-  Prateleira* pratOrigem = &(prateleiras[prat_origem]);
-  Pilha* livrosOrigem = &(pratOrigem->pilhas[col_origem]);
-  
-  Prateleira* pratDestino = &(prateleiras[prat_destino]);
-  Pilha* livrosDestino = &(pratDestino->pilhas[col_destino]);
-
-  Pilha aux;
-  inicializarPilha(&aux,pratOrigem->limite);
-  while (livrosOrigem->tamanho > 0){
-    Livro* livro = desempilhar(&(*livrosOrigem));
-    empilhar(&aux,*livro);
+/*
+  Desempilhamos todos os livros de uma determinada coluna de uma determinada prateleira de um preço X para uma outra
+  coluna de outra determinada prateleira de um outro preço Y, utilizando o compartimento auxiliar para manter 
+  a ordem de empilhamento.
+*/
+void mudarPrecos(Prateleira* prateleiras, int prat_origem, int col_origem, int prat_destino, int col_destino, Pilha* aux){
+  while (prateleiras[prat_origem].pilhas[col_origem].tamanho > 0){
+    Livro* livro = desempilhar(&(prateleiras[prat_origem].pilhas[col_origem]));
+    prateleiras[prat_origem].qtdLivros--;
+    empilhar(&(*aux),*livro);
   }
-  while (aux.tamanho > 0){
-    Livro* livro = desempilhar(&aux);
-    empilhar(&(*livrosDestino),*livro);
+  while (aux->tamanho > 0){
+    Livro* livro = desempilhar(&(*aux));
+    empilhar(&(prateleiras[prat_destino].pilhas[col_destino]),*livro);
+    prateleiras[prat_destino].qtdLivros++;
   }
 }
 
-void reorganizarPrateleira(Prateleira* prateleiras, int prat, int* configuracao){
-  Prateleira* prateleira = &(prateleiras[prat]);
+/*
+  Reorganizamos as colunas de uma determinada prateleira informada, com base numa nova configuração
+  de índices informada. Para isso, iremos trocar a pilha que está na posição informada pela 
+  nova configuração pela pilha que estamos percorrendo. Ou seja, se configuracao[i] = x, logo 
+  a coluna que está na posição X receberá a pilha que está na coluna de posição i, para todas
+  as posições i da nossa prateleira.
+*/
+void reorganizarPrateleira(Prateleira* prateleiras, int prat, int* configuracao, Pilha* aux){
+  Prateleira pratAux;
+  inicializarPrateleira(&pratAux,prateleiras[prat].colunas,prateleiras[prat].limite);
+
   int i;
-  for (i = 0; i < prateleira->colunas; i++){
-    Pilha* livrosA = &(prateleira->pilhas[i]);
-    Pilha* livrosB = &(prateleira->pilhas[configuracao[i]]);
-    Pilha aux;
-    inicializarPilha(&aux,livrosA->limite);
-    while (livrosA->tamanho > 0){
-      Livro* livro = desempilhar(&(*livrosA));
-      empilhar(&aux,*livro);
+  for (i = 0; i < prateleiras[prat].colunas; i++){
+    while (prateleiras[prat].pilhas[i].tamanho > 0){
+      Livro* livro = desempilhar(&(prateleiras[prat].pilhas[i]));
+      empilhar(&(pratAux.pilhas[configuracao[i]]),*livro);
     }
-    while (livrosB->tamanho > 0){
-      Livro* livro = desempilhar(&(*livrosB));
-      empilhar(&aux,*livro);
-    }
-    while (aux.tamanho > livrosA->tamanho){
-      Livro* livro = desempilhar(&aux);
-      empilhar(&(*livrosA),*livro);
-    }
-    while (aux.tamanho > 0){
-      Livro* livro = desempilhar(&aux);
-      empilhar(&(*livrosB),*livro);
+  }
+  for (i = 0; i < prateleiras[prat].colunas; i++){
+    while (pratAux.pilhas[i].tamanho > 0){
+      Livro* livro = desempilhar(&(pratAux.pilhas[i]));
+      empilhar(&(prateleiras[prat].pilhas[i]),*livro);
     }
   }
 }
 
+/*
+  Calculamos e exibimos o preço total de todos livros em cada prateleira, que será o nosso 
+  montante parcial de cada prateleira (número de livros na prateleira multiplicado
+  pelo preço referente a cada livro naquela prateleira). Também calculamos e exibimos o 
+  montante total referente à estente, que será a somatória de todos os montantes parciais
+  de cada uma das prateleiras.
+*/
 void calcularMontante(Prateleira* prateleiras, int qtdPrateleiras){
   float montantes[qtdPrateleiras];
   float montanteTotal = 0;
   int i;
   for (i = 0; i < qtdPrateleiras; i++){
-    Prateleira prateleira = prateleiras[i];
-    montantes[i] = prateleira.qtdLivros * prateleira.preco;
+    montantes[i] = prateleiras[i].qtdLivros * prateleiras[i].preco;
     montanteTotal += montantes[i];
   }
   printf("[VALOR TOTAL DA ESTANTE $%.0f] dos quais:\n",montanteTotal);
